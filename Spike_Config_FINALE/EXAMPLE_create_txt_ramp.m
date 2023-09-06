f4990a9c194416aef889b1ef595a4265b6977dd2
Paolo Tecchio <73119114+PaulT95@users.noreply.github.com>
2023-09-06 08:51:59 +0200

    Bug Fixed for plotting ramps range
    + Bug Fixed for plotting ramps or traces range
    + Auto adjust y axis in XY automatically by using optimise and no calculation
    + Add example python script for create txt ramp

--- Spike_Config_FINALE/EXAMPLE_create_txt_ramp.m
+++ Spike_Config_FINALE/EXAMPLE_create_txt_ramp.m
@@ -1,15 +1,17 @@
-%%Simple script to create a matrix with 2 columns to be plotted in Spike2 as a trace
-% Col 1 = x (time points), Col 2 = y (whatever)
+%%  Simple script to create a matrix with 2 columns to be plotted in Spike2 as a trace
 %%%%%
 % Author: Paolo Tecchio
-% Email: paolo.tecchio@rub.de
+
 clear
 clc
+
+% Col 1 = x (time points), Col 2 = y (whatever)
+
 %must be columns
 %x points or time points
-matrix(:,1) = linspace(0,40,1000); 
+matrix(:,1) = linspace(0,40,1000);
 
-%y points 
+%y points
 matrix(:,2) = sin(matrix(:,1))*20; %20 is the amplitude
 matrix = round(matrix,2);
 plot(matrix(:,1),matrix(:,2));
@@ -17,7 +19,3 @@ plot(matrix(:,1),matrix(:,2));
 %save the txt
 [fname,pth] = uiputfile('*.txt'); % Type in name of the file.
 writematrix(matrix,[pth fname]);
-
-
-
-
